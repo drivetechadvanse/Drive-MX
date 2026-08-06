03d3e871687a7725caa6ed13eaacaefb71e9ee13
drivetechnology <ar92.71glvz@gmail.com>
2026-08-06 05:46:30 -0600

    Delete guide-number-entry directory

--- guide-number-entry/guide-number-entry.js
+++
@@ -1,9 +0,0 @@
-import { EnterGuideNumberCard } from './components/EnterGuideNumberCard.js';
-import * as GuideNumberEntryServices from './services/guideNumberEntryService.js';
-
-export { EnterGuideNumberCard, GuideNumberEntryServices };
-
-globalThis.DriveMxGuideNumberEntry = {
-  EnterGuideNumberCard,
-  services: GuideNumberEntryServices
-};
